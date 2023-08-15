b7d0729abdbf1e49f054f8d095a5d7c0ef921249
Laurent De La Calle <laurentdelacalle@hotmail.com>
2023-08-15 11:27:32 +0200

    setting up Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Housing from "./pages/Housing/Housing";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Error from "./components/ErrorPage/Error";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter ( [
+  {
+    path:"/",
+    element:<Layout/>,
+    errorElement:<Error/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>,
+      },
+      {
+        path:"/housing/:id",
+        element:<Housing/>,
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+    ]
+  },
+  
+])
+
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+     <RouterProvider router={router} />
+    </>
   );
 }
 
